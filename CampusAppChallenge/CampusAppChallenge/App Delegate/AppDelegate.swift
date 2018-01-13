1c0f7db4ecf29e67714c18f2a388c3e85bc34ed8
RMalczynski <rafal.malczynski@gmail.com>
2018-01-13 16:36:39 +0100

    coś sie jebe

--- CampusAppChallenge/CampusAppChallenge/App Delegate/AppDelegate.swift
+++ CampusAppChallenge/CampusAppChallenge/App Delegate/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import IndoorwaySdk
+import OAuthSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -30,15 +31,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func applicationDidFinishLaunching(_ aNotification: NSNotification) {
-        NSAppleEventManager.shared().setEventHandler(self, andSelector:#selector(AppDelegate.handleGetURL(event:withReplyEvent:)), forEventClass: AEEventClass(kInternetEventClass), andEventID: AEEventID(kAEGetURL))
-    }
-    
-    func handleGetURL(event: NSAppleEventDescriptor!, withReplyEvent: NSAppleEventDescriptor!) {
-        if let urlString = event.paramDescriptor(forKeyword: AEKeyword(keyDirectObject))?.stringValue, let url = URL(string: urlString) {
-            OAuthSwift.handle(url: url)
-        }
-    }
+//    func applicationDidFinishLaunching(_ aNotification: NSNotification) {
+//        NSAppleEventManager.shared().setEventHandler(self, andSelector:#selector(AppDelegate.handleGetURL(event:withReplyEvent:)), forEventClass: AEEventClass(kInternetEventClass), andEventID: AEEventID(kAEGetURL))
+//    }
+//    
+//    func handleGetURL(event: NSAppleEventDescriptor!, withReplyEvent: NSAppleEventDescriptor!) {
+//        if let urlString = event.paramDescriptor(forKeyword: AEKeyword(keyDirectObject))?.stringValue, let url = URL(string: urlString) {
+//            OAuthSwift.handle(url: url)
+//        }
+//    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
