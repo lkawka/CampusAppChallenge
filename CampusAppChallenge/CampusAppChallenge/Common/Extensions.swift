3d9349b0944302a90a3dccf634f108fa94d58631
RMalczynski <rafal.malczynski@gmail.com>
2018-01-13 20:11:02 +0100

    ? xD

---
+++ CampusAppChallenge/CampusAppChallenge/Common/Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Extensions.swift
+//  CampusAppChallenge
+//
+//  Created by Rafał Małczyński on 13.01.2018.
+//  Copyright © 2018 Lukasz Kawka. All rights reserved.
+//
+
+import Foundation
+
+extension Date
+{
+    convenience
+    init(dateString:String) {
+        let dateStringFormatter = DateFormatter()
+        dateStringFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        dateStringFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        let d = dateStringFormatter.dateFromString(dateString)!
+        self.init(timeInterval:0, sinceDate:d)
+    }
+}
