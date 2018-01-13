cb0d51c286add1e9b21c67f6b4afe89bb50794a4
lkawka <luk.kawka@gmail.com>
2018-01-13 13:35:16 +0100

    Adding project file

---
+++ CampusAppChallenge/CampusAppChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CampusAppChallenge
+//
+//  Created by Lukasz Kawka on 13/01/2018.
+//  Copyright © 2018 Lukasz Kawka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
